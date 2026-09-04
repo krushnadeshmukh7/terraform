0d497b9d9e82c98702dd31d7c4190e2ff2745242
krushnadeshmukh7 <kdeshmukh2@gmail.com>
2026-09-04 09:32:32 +0530

    day2

--- day_2_vpc/var.tf
+++ day_2_vpc/var.tf
@@ -1,49 +1,39 @@
 variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
-  default     = "0,0.0.0/16"
-} 
-
-variable "public_cidr" {
-  description = "CIDR block for the public subnet"
-  default     = "0,0.0.0/20"
+    default = "10.0.0.0/16"
 }
 
-vairable "private_cidr" {
-  description = "CIDR block for the private subnet"
-  default     = "0,0.0.0/20"
+variable "public_cidr" {
+    default = "10.0.0.0/20"
 }
 
 variable "public_az" {
-  description = "Availability zone for the public subnet"
-  default     = "us-east-1a"
+    default = "us-west-2a"
+}
+
+variable "private_cidr" {
+    default = "10.0.16.0/20"
 }
 
 variable "private_az" {
-  description = "Availability zone for the private subnet"
-  default     = "us-east-1b"
+    default = "us-west-2b"
 }
 
-variable "ami_id" {
-  description = "AMI ID for the EC2 instance"
-   default = "ami-0bea529386a62a2ad"
+variable "ami" {
+    default = "ami-0bea529386a62a2ad"
 }
 
 variable "instance_type" {
-  description = "Instance type for the EC2 instance"
-  default     = "t3.micro"
+    default = "t3.micro"
 }
 
 variable "key_name" {
-  description = "Key pair name for the EC2 instance"
-  default     = "sarika"
+    default = "krushna"
 }
 
-vairable "volume_size" {
-  description = "Size of the EBS volume in GB"
-  default     = 10
+variable "volume_size" {
+    default = 10
 }
 
-vairable "volume_type" {
-  description = "Type of the EBS volume"
-  default     = "gp3"
+variable "volume_type" {
+    default = "gp3"
 }
