d85865d176f727a6cc10e0c9119078c11299c732
krushnadeshmukh7 <kdeshmukh2@gmail.com>
2026-09-08 10:28:03 +0530

    terraform

--- day-5-terraform-var-file/module/ec2/output.tf
+++ day-5-terraform-var-file/module/ec2/output.tf
@@ -1,8 +1,7 @@
 output "public_ip" {
-    value = aws_instance.my_ec2.public_ip
+    value = aws_instance.public_instance.public_ip
 }
 
 output "private_ip" {
-    value = aws_instance.my_ec2.private_ip
+    value = aws_instance.private_instance.private_ip
 }
-
