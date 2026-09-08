fb817a84030035c42cd343667847e41af53aa67f
krushnadeshmukh7 <kdeshmukh2@gmail.com>
2026-09-08 13:17:21 +0530

    file

---
+++ day-6-tfstate/provier.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+    region = "ap-east-1"
+    profile = "dev"
+  
+}
+
+terraform {
+  backend "s3" {
+    bucket = "krushna-s3-123123"
+    region = "ap-east-1"
+    profile = "dev"
+    use_lockfile = true
+    key = krushna
+    shared_credentials_files = [ "./root/.aws/credentials" ]
+  }
+}
